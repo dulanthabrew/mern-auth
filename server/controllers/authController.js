a56bca7bbf9359c3e16a20f30dfbd391bb75b5a0
dulanthabrew <dulanonline895@gmail.com>
2025-09-14 08:17:41 +0530

    implemented password reset option and added gitignore file

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -177,3 +177,41 @@ export const isAuthenticated = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// Send password reset OTP
+export const sendResetOtp = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.json({ success: false, message: "Email is required" });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.resetOtp = otp;
+    user.resetOtpExpireAt = Date.now() + 15 * 60 * 1000;
+
+    await user.save();
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email,
+      subject: "Password reset OTP",
+      text: `Your OTP for resetting your password is ${otp}. Use this OTP tp proceed with resetting your password.`,
+    };
+    await transporter.sendMail(mailOptions);
+
+    return res.json({ success: true, message: "OTP sent to your email" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// Reset user password
+export const resetPassword = async (req, res) => {};
